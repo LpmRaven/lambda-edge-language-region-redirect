e3aae96e5b97dfd71ce7854a2fd77d35ecb2205b
LpmRaven <liamraven@gmail.com>
2020-12-08 16:41:54 +1100

    adds tests for custom response

--- helpers/tests/get-custom-response-with-url.test.js
+++ helpers/tests/get-custom-response-with-url.test.js
@@ -9,11 +9,10 @@ describe('check required fields', () => {
         jest.resetAllMocks();
     });
 
-    test('will return ', async () => {
-        // let mockEvent = mockOriginRequestEvent;
-        // mockEvent.Records[0].cf.request.uri = "";
+    test('will return ccustom response with new Location value', async () => {
+        const url = "/en-gb/test-url";
 
-        // const result = await handler(mockEvent);
-        // expect(result).toEqual(mockEvent.Records[0].cf.request);
+        const result = await getCustomResponseWithUrl(url);
+        expect(result).toEqual({ "headers": { "cache-control": [{ "key": "Cache-Control", "value": "max-age=3600" }], "location": [{ "key": "Location", "value": "/en-gb/test-url" }] }, "status": "302", "statusDescription": "Found" });
     });
 })
