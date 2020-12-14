d68228804f6860dcd4064518346d566018ec3b5f
LpmRaven <liamraven@gmail.com>
2020-12-14 13:27:24 +1100

    adds more tests for root dir

--- helpers/tests/get-custom-response-with-url.test.js
+++ helpers/tests/get-custom-response-with-url.test.js
@@ -9,10 +9,24 @@ describe('get-custom-response-with-url', () => {
         jest.resetAllMocks();
     });
 
-    test('will return ccustom response with new Location value', async () => {
+    test('will return custom response with new Location value', async () => {
         const url = "/en-gb/test-url";
 
         const result = await getCustomResponseWithUrl(url);
         expect(result).toEqual({ "headers": { "cache-control": [{ "key": "Cache-Control", "value": "max-age=3600" }], "location": [{ "key": "Location", "value": "/en-gb/test-url/index.html" }] }, "status": "302", "statusDescription": "Found" });
+    });
+
+    test('will return custom response with new Location value for root', async () => {
+        const url = "/index.html";
+
+        const result = await getCustomResponseWithUrl(url);
+        expect(result).toEqual({ "headers": { "cache-control": [{ "key": "Cache-Control", "value": "max-age=3600" }], "location": [{ "key": "Location", "value": "/index.html" }] }, "status": "302", "statusDescription": "Found" });
+    });
+
+    test('will return custom response with new Location value for dir with index', async () => {
+        const url = "/en-gb/index.html";
+
+        const result = await getCustomResponseWithUrl(url);
+        expect(result).toEqual({ "headers": { "cache-control": [{ "key": "Cache-Control", "value": "max-age=3600" }], "location": [{ "key": "Location", "value": "/en-gb/index.html" }] }, "status": "302", "statusDescription": "Found" });
     });
 })
